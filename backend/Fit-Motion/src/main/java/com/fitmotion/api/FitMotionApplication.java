4b0987b8c31c617e9e035793768f1983473b17a3
Wesley Martins <wesleymartinsjnr@gmail.com>
2024-07-23 15:51:00 -0300

    first commit

---
+++ backend/Fit-Motion/src/main/java/com/fitmotion/api/FitMotionApplication.java
@@ -0,0 +1,13 @@
+package com.fitmotion.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitMotionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitMotionApplication.class, args);
+	}
+
+}
